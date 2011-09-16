3236ee03d55481bf5d6e3031bd1575541549614b
Joachim Hofer <jmhofer.github@johoop.de>
2011-09-16 22:17:48 +0200

    moved two more keys to their own trait

--- src/main/scala/de/johoop/jacoco4sbt/JacocoPlugin.scala
+++ src/main/scala/de/johoop/jacoco4sbt/JacocoPlugin.scala
@@ -6,20 +6,16 @@ import Keys._
 object JacocoPlugin extends Plugin with Commands {
 
   object jacoco extends Commands {
-    lazy val Config = config("jacoco") hide
   
     val dependencies = Seq(
       "org.jacoco" % "org.jacoco.agent" % "0.5.3.201107060350" % "jacoco" artifacts(Artifact("org.jacoco.agent", "jar", "jar")),
       "org.jacoco" % "org.jacoco.core" % "0.5.3.201107060350" % "jacoco" artifacts(Artifact("org.jacoco.core", "jar", "jar")),
       "org.jacoco" % "org.jacoco.report" % "0.5.3.201107060350" % "jacoco" artifacts(Artifact("org.jacoco.report", "jar", "jar")))
   
-  
     def unpackAgentAction(libManagedJacoco: File, classpath: Classpath) = {
       val outerAgentJar = classpath.files find (_.getName contains "agent")
       IO.unzip(outerAgentJar.get, libManagedJacoco, "*.jar").head
     }
-  
-    lazy val jacocoClasspath = TaskKey[Classpath]("jacoco-classpath")
   
     val settings : Seq[Setting[_]] = Seq(
       commands += jacocoCommand,
@@ -27,6 +23,5 @@ object JacocoPlugin extends Plugin with Commands {
       libraryDependencies ++= dependencies,
       jacocoClasspath <<= (classpathTypes, update) map { Classpaths managedJars (Config, _, _) },
       unpackAgent <<= (managedDirectory in Config, jacocoClasspath) map unpackAgentAction)
-  
   }
 }
--- src/main/scala/de/johoop/jacoco4sbt/Keys.scala
+++ src/main/scala/de/johoop/jacoco4sbt/Keys.scala
@@ -1,7 +1,12 @@
 package de.johoop.jacoco4sbt
 
 import sbt._
+import Keys._
 
 trait Keys {
+  lazy val Config = config("jacoco") hide
+  
+  lazy val jacocoClasspath = TaskKey[Classpath]("jacoco-classpath")
+  
   lazy val unpackAgent = TaskKey[File]("unpack-jacoco-agent", "Unpacks the Jacoco Agent JAR")
 }
