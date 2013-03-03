7561207e97debc28ba178e414b2b5eda2e3c37cc
Joachim Hofer <jmhofer.github@johoop.de>
2013-03-03 10:21:06 +0100

    removed file walker helper (it's built into sbt as PathFinder)

--- src/main/scala/de/johoop/jacoco4sbt/Merging.scala
+++ src/main/scala/de/johoop/jacoco4sbt/Merging.scala
@@ -26,15 +26,15 @@ trait Merging extends JaCoCoRuntime {
   def mergeAction(jacocoDirectory: File, streams: TaskStreams) = {
     val parent = jacocoDirectory.getParentFile
 
-    import FileSeq._
-    val execs = parent.walkWithFilter(_.getName == "jacoco.exec")
-    streams.log debug ("Found data files: %s" format execs.map(_.getAbsolutePath).mkString(","))
+    val execs = (parent ** "jacoco.exec").get
+    streams.log debug ("Found data files: %s" format execs.map(_.absolutePath).mkString(", "))
 
     val loader = new ExecFileLoader
     execs foreach loader.load
 
     val mergedFile = new File(jacocoDirectory, "jacoco-merged.exec")
     streams.log debug ("Writing merged data to: %s" format mergedFile.getAbsolutePath)
+    
     writeToFile(mergedFile) { outputStream =>
       val dataWriter = new ExecutionDataWriter(outputStream)
       loader.getSessionInfoStore accept dataWriter
@@ -52,29 +52,6 @@ trait Merging extends JaCoCoRuntime {
     } catch {
       case e: IOException => 
         throw new ResourcesException("Unable to write out Jacoco file during merge: %s" format e.getMessage)
-    }
-  }
-}
-
-// Helper classes to easily walk through trees
-object FileSeq {
-  def apply(dir:File) = new FileSeq(dir.listFiles().toList)
-
-  // implicit conversion of File to FileSeq
-  implicit def file2FileSeq(dir:File):FileSeq = FileSeq(dir)
-
-  class FileSeq(override val files:List[File]) extends GenFileList(files)
-
-  import collection.GenSeq
-  class GenFileList(val files:GenSeq[File]) {
-    private def fileOrSubDir: (File) => GenSeq[File] = f => if (f.isFile) GenSeq(f) else f.walk
-
-    def walk:GenSeq[File] = {
-      files.flatMap(fileOrSubDir)
-    }
-
-    def walkWithFilter(fileFilter: File => Boolean):GenSeq[File] = {
-      files.filter(f => f.isDirectory || fileFilter(f)).flatMap(fileOrSubDir).filter(fileFilter)
     }
   }
 }
