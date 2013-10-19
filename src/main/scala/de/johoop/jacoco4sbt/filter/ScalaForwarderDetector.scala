2c9a77817f0bc60b4a07ac50e28bce739dec58d5
Joachim Hofer <jmhofer.github@johoop.de>
2013-10-19 16:35:44 +0200

    added comment headers
    Signed-off-by: Joachim Hofer <jmhofer.github@johoop.de>

--- src/main/scala/de/johoop/jacoco4sbt/filter/ScalaForwarderDetector.scala
+++ src/main/scala/de/johoop/jacoco4sbt/filter/ScalaForwarderDetector.scala
@@ -1,3 +1,14 @@
+/*
+ * This file is part of jacoco4sbt.
+ *
+ * Copyright (c) 2013 Joachim Hofer & contributors
+ * All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package de.johoop.jacoco4sbt.filter
 
 import org.objectweb.asm.Opcodes
