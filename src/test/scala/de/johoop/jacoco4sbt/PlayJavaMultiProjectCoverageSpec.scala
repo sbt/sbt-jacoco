f2fc7bb64d7d981b4e2f95399fe6d16618d45a8b
Joachim Hofer <jmhofer.github@johoop.de>
2013-12-21 17:08:06 +0100

    added a test checking that the tests of the multi-project succeed

--- src/test/scala/de/johoop/jacoco4sbt/PlayJavaMultiProjectCoverageSpec.scala
+++ src/test/scala/de/johoop/jacoco4sbt/PlayJavaMultiProjectCoverageSpec.scala
@@ -4,7 +4,8 @@ import org.specs2._
 import de.johoop.jacoco4sbt.build.BuildInfo
 import scala.sys.process.Process
 import java.io.File
-import org.specs2.matcher.FileMatchers
+import org.specs2.matcher.{Matcher, FileMatchers}
+import scala.xml.{NodeSeq, XML}
 
 class PlayJavaMultiProjectCoverageSpec extends Specification with FileMatchers { def is = s2"""
   $sequential
@@ -13,7 +14,8 @@ class PlayJavaMultiProjectCoverageSpec extends Specification with FileMatchers {
   Covering tests in a Play Scala project with subprojects should
     return an exit code of zero,                                  $e1
     create a jacoco target directory in each subproject,          $e2
-    create a classes directory in each subproject.                $e3
+    create a classes directory in each subproject,                $e3
+    not fail any executed test cases.                             $e4
 """
 
   lazy val testProjectBase = new File(BuildInfo.test_resourceDirectory, "play-multi-project-jacoco")
@@ -21,10 +23,18 @@ class PlayJavaMultiProjectCoverageSpec extends Specification with FileMatchers {
   lazy val targetDirs = targets map (new File(testProjectBase, _))
   lazy val jacocoDirs = targetDirs map (new File(_, "scala-2.10/jacoco"))
   lazy val coveredClassesDirs = jacocoDirs map (new File(_, "classes"))
+  lazy val testReports = for {
+    reportPath <- List("Application", "Integration") map ("test-reports/" + _ + "Test.xml")
+    targetDir <- targetDirs
+  } yield new File(targetDir, reportPath)
 
   lazy val exitCode = Process("sbt.bat clean jacoco:cover", Some(testProjectBase)) !
 
   def e1 = exitCode should be equalTo(0)
-  def e2 = jacocoDirs must contain(beADirectory).foreach
-  def e3 = coveredClassesDirs must contain(beADirectory).foreach
+  def e2 = jacocoDirs should contain(beADirectory).foreach
+  def e3 = coveredClassesDirs should contain(beADirectory).foreach
+
+  def e4 = testReports should contain(onlySuccesses).foreach
+
+  def onlySuccesses: Matcher[File] = { file: File => XML.loadFile(file) \\ "failure" should beEmpty }
 }
