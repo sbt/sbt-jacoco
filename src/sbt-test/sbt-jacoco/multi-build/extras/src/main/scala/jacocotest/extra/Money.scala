bb8da912053938e36b30ab6774d1ca17d373f223
Michael Stringer <stringbean@users.noreply.github.com>
2017-09-08 10:56:02 +0100

    Fix locale issue with scripted tests

--- src/sbt-test/sbt-jacoco/multi-build/extras/src/main/scala/jacocotest/extra/Money.scala
+++ src/sbt-test/sbt-jacoco/multi-build/extras/src/main/scala/jacocotest/extra/Money.scala
@@ -1,9 +1,9 @@
 package jacocotest.extra
 
-import java.util.Currency
+import java.util.{Currency, Locale}
 
 case class Money(amount: BigDecimal, currency: Currency) {
   override def toString: String = {
-    "%.2f %s".format(amount, currency.getSymbol)
+    "%.2f %s".format(amount, currency.getSymbol(Locale.forLanguageTag("en-GB")))
   }
 }
