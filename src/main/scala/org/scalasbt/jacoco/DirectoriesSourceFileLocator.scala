fb2664d65421e3374ffe02f57d848dcc85bbec20
Michael Stringer <stringbean@users.noreply.github.com>
2017-09-08 09:26:12 +0100

    Rename package

---
+++ src/main/scala/org/scalasbt/jacoco/DirectoriesSourceFileLocator.scala
@@ -0,0 +1,32 @@
+/*
+ * This file is part of jacoco4sbt.
+ *
+ * Copyright (c) Joachim Hofer & contributors
+ * All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.scalasbt.jacoco
+
+import java.io.{File, Reader}
+
+import org.jacoco.report.{DirectorySourceFileLocator, ISourceFileLocator}
+import org.scalasbt.jacoco.report.JacocoSourceSettings
+
+class DirectoriesSourceFileLocator(directories: Seq[File], sourceSettings: JacocoSourceSettings)
+    extends ISourceFileLocator {
+
+  override def getSourceFile(packageName: String, fileName: String): Reader = {
+    def findInDirectory(dir: File) = Option(dirSourceLocator(dir).getSourceFile(packageName, fileName))
+    def dirSourceLocator(dir: File) =
+      new DirectorySourceFileLocator(dir, sourceSettings.fileEncoding, sourceSettings.tabWidth)
+
+    (directories flatMap findInDirectory).headOption.orNull
+  }
+
+  override def getTabWidth: Int = sourceSettings.tabWidth
+}
---
+++ src/main/scala/org/scalasbt/jacoco/report/formats/ScalaHTMLReportFormat.scala
@@ -0,0 +1,73 @@
+/*
+ * This file is part of jacoco4sbt.
+ *
+ * Copyright (c) Joachim Hofer & contributors
+ * All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.scalasbt.jacoco.report.formats
+
+import java.io.File
+
+import org.jacoco.core.analysis.ICoverageNode
+import org.jacoco.report.html.HTMLFormatter
+import org.jacoco.report.internal.html.resources.Styles
+import org.jacoco.report.internal.html.table._
+import org.jacoco.report.{FileMultiReportOutput, IReportVisitor}
+import org.scalasbt.jacoco.ScalaLanguageNames
+
+class ScalaHTMLReportFormat(withBranchCoverage: Boolean = true) extends JacocoReportFormat {
+  override def createVisitor(directory: File, encoding: String): IReportVisitor = {
+    val formatter = new ScalaHtmlFormatter(withBranchCoverage)
+    formatter.setOutputEncoding(encoding)
+    formatter.createVisitor(new FileMultiReportOutput(new File(directory, "html")))
+  }
+
+  def withoutBranches: ScalaHTMLReportFormat = new ScalaHTMLReportFormat(withBranchCoverage = false)
+}
+
+/**
+  * Omits displaying instruction and branch coverage in the coverage tables,
+  * as Scala generates null checks which make these too noisy.
+  *
+  * TODO: Find a way to remove them from the annotated source code reports, too.
+  */
+class ScalaHtmlFormatter(withBranchCoverage: Boolean) extends HTMLFormatter {
+  private var table: Option[Table] = None
+
+  setLanguageNames(new ScalaLanguageNames)
+
+  override def getTable: Table = {
+    table getOrElse {
+      val newTable = createTable
+      table = Some(newTable)
+      newTable
+    }
+  }
+
+  private def createTable: Table = {
+    val t: Table = new Table
+    t.add("Element", null, new LabelColumn, false)
+    t.add("Missed Lines", Styles.BAR, new BarColumn(ICoverageNode.CounterEntity.LINE, getLocale), true)
+    t.add("Total Lines", Styles.CTR1, CounterColumn.newTotal(ICoverageNode.CounterEntity.LINE, getLocale), false)
+    t.add("Cov.", Styles.CTR2, new PercentageColumn(ICoverageNode.CounterEntity.LINE, getLocale), false)
+    if (withBranchCoverage) {
+      t.add("Missed Branches", Styles.BAR, new BarColumn(ICoverageNode.CounterEntity.BRANCH, getLocale), false)
+      t.add("Cov.", Styles.CTR2, new PercentageColumn(ICoverageNode.CounterEntity.BRANCH, getLocale), false)
+    }
+    addMissedTotalColumns(t, "Methods", ICoverageNode.CounterEntity.METHOD)
+    addMissedTotalColumns(t, "Classes", ICoverageNode.CounterEntity.CLASS)
+
+    t
+  }
+
+  private def addMissedTotalColumns(table: Table, label: String, entity: ICoverageNode.CounterEntity): Unit = {
+    table.add("Missed", Styles.CTR1, CounterColumn.newMissed(entity, getLocale), false)
+    table.add(label, Styles.CTR2, CounterColumn.newTotal(entity, getLocale), false)
+  }
+}
