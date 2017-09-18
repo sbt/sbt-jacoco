33d8516628ea5ec832f857a67ae396ffa3986bea
Michael Stringer <stringbean@users.noreply.github.com>
2017-09-18 12:54:58 +0100

    Tidy reporting helpers

--- src/main/scala/org/scalasbt/jacoco/data/ExecutionDataUtils.scala
+++ src/main/scala/org/scalasbt/jacoco/data/ExecutionDataUtils.scala
@@ -22,13 +22,12 @@ import sbt.Keys.TaskStreams
 import sbt._
 
 object ExecutionDataUtils {
-  def saveRuntimeData(data: RuntimeData, dest: File, forked: Boolean, streams: TaskStreams): Unit = {
-
+  def saveRuntimeData(data: RuntimeData, destination: File, forked: Boolean, streams: TaskStreams): Unit = {
     if (!forked) {
-      streams.log.debug(s"writing execution data to $dest")
-      IO.createDirectory(dest.getParentFile)
+      streams.log.debug(s"writing execution data to $destination")
+      IO.createDirectory(destination.getParentFile)
 
-      for (os <- managed(new FileOutputStream(dest))) {
+      for (os <- managed(new FileOutputStream(destination))) {
         val executionDataWriter = new ExecutionDataWriter(os)
         data.collect(executionDataWriter, executionDataWriter, true)
       }
