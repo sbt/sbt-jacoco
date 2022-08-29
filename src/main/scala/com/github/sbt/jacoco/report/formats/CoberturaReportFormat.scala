6ce5c4867cbfcc96e4f1d5bb8036e6736c84c04a
Aleksandr Nekrasov <aleksandr.nekrasov@agoda.com>
2022-08-29 16:50:40 +0700

    header added to cobertura report formatter

--- src/main/scala/com/github/sbt/jacoco/report/formats/CoberturaReportFormat.scala
+++ src/main/scala/com/github/sbt/jacoco/report/formats/CoberturaReportFormat.scala
@@ -1,9 +1,20 @@
+/*
+ * This file is part of sbt-jacoco.
+ *
+ * Copyright (c) Joachim Hofer & contributors
+ * All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package com.github.sbt.jacoco.report.formats
 
 import com.github.sbt.jacoco.report.DirectoriesSourceFileLocator
 import org.jacoco.core.analysis.{IBundleCoverage, IClassCoverage, IMethodCoverage, IPackageCoverage}
 import org.jacoco.core.data.{ExecutionData, SessionInfo}
-import org.jacoco.report.internal.xml.XMLGroupVisitor
 import org.jacoco.report.{IReportGroupVisitor, IReportVisitor, ISourceFileLocator, JavaNames}
 import sbt.IO
 
